51e7a3dfc6eb7b88ad2ef5119800ffa5f3312410
Claudia dos Santos <aclaudia.santosd@gmail.com>
2026-06-18 13:59:24 +0200

    cambiado codigo de conexion a bd para despliegue

--- js/conexion-bd.js
+++ js/conexion-bd.js
@@ -1,10 +1,11 @@
 let mysql = require("mysql");
 
 let conexion = mysql.createConnection({
-    host: "localhost",
-    database: "plantas_db",
-    user:"root",
-    password:""
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
 });
 
 conexion.connect(function(err){
